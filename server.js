2fe7877b75326e382b77c1d140c9ec8e3b5f961c
LaskyLothbrok <Shibarai95@gmail.com>
2022-07-15 15:00:46 +0200

    port fixed

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const port = process.env.PORT || 8000
 app = express()
 app.use(express.static(__dirname + '/public'))
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 
 })
-app.listen(8000, () => {
+app.listen(port, () => {
     console.log('Server up and running')
 })
